96e97bf95d5785cac228fd2d1b9d9522ecd9af8b
Miyagoshi_Sota <miyagoshisouta@gmail.com>
2023-11-06 16:05:49 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import Recording from './components/recording.jsx'
 import Header from './components/header.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header/>
-      <Recording/>
     </>
   )
 }
