d1e2648701835c519e60e702c9a6f50f4b39444a
Miyagoshi_Sota <miyagoshisouta@gmail.com>
2023-11-08 16:07:27 +0900

    substruct gurbage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import Recording from './components/recording.jsx'
 import Header from './components/header.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header/>
