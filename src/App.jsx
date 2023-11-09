9481f8f8ccd976446f55929aede3ad999d82c7b4
Miyagoshi_Sota <miyagoshisouta@gmail.com>
2023-11-09 18:23:44 +0900

    add loginScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Recording from './components/recording.jsx'
 import Header from './components/header.jsx'
+import Rogin from "./components/login.jsx"
+import { useState } from 'react'
 
 function App() {
+  const [login, isLogin] = useState(false)
+  const successLogin = () => {
+    isLogin(true)
+  }
+
   return (
     <>
-      <Header/>
-      <Recording/>
+      {!login && <Rogin onChildEvent = {successLogin}/>}
+      {login && <Header/>}
+      {login && <Recording/>}
     </>
   )
 }
