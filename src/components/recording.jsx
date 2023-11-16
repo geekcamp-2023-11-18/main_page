4333edb3992677238f6299fd325bea7870e3a62b
ogasawara-k23024 <ourinhu269@gmail.com>
2023-11-16 18:43:01 +0900

    録画追加

--- src/components/recording.jsx
+++ src/components/recording.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./recording.css";
 import sky from "../imgs/sky.jpg";
 import nightsky from "../imgs/nightsky.jpg";
@@ -6,6 +6,8 @@ import tetsuya from "../imgs/blueButton.png";
 import otsu from "../imgs/redButton.png";
 //import useSound from "use-sound"; いらなくなった
 import soundDoor from "../musics/doorOpen.mp3";
+import { ReactMediaRecorder } from "react-media-recorder";
+import "canvas-toBlob";
 
 let button = tetsuya;
 let changer = 0;
@@ -34,14 +36,21 @@ function recording() {
     oto.play();
     //useSound(soundDoor); いらなくなった
 
-     if (changer === 0) {
-       changer = 1;
-       setButton(otsu);
-     } else {
-       changer = 0;
-       setButton(tetsuya);
-     }
+    if (changer === 0) {
+      changer = 1;
+      setButton(otsu);
+    } else {
+      changer = 0;
+      setButton(tetsuya);
+    }
   }
+
+  const changeFile = (blobURL) => {
+    const file = new File([blobURL], 'sample.mp4', {type: blobURL.type})
+    //const video = document.getElementById("aaaa");
+    //video.setAttribute("src",file)
+    console.log(file)
+  };
 
   //HTML
   return (
@@ -62,6 +71,20 @@ function recording() {
         </div>
       </div>
       <img src={button} className="button" onClick={change} />
+      <div className="rokuga">
+        <ReactMediaRecorder
+          video
+          render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+            <div className="rokuga">
+              <p>{status}</p>
+              <button onClick={startRecording}>Start Recording</button>
+              <button onClick={stopRecording}>Stop Recording</button>
+              <button onClick={() => changeFile(mediaBlobUrl)}>Change File</button>
+              <video  controls autoPlay loop />
+            </div>
+          )}
+        />
+      </div>
     </div>
   );
 }
